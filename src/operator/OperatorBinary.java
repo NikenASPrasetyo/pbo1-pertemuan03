17ff15ec18a12a2d3a3deeeeb2682ab3906ab701
NikenASPrasetyo <nikenasprasetyo@gmail.com>
2020-04-12 19:02:21 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai ke dalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Niken AS Prasetyo";
+    int jumlahSks = 24;
+    double IPK = 3.98;
+
+}
