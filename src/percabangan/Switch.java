73f3a80a02e440f4fd22acb16f3aeb0fdef6908d
NikenASPrasetyo <nikenasprasetyo@gmail.com>
2020-04-13 15:47:21 +0800

    Switch

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,20 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int makanan = 2;
+        switch(makanan){
+            case 1:
+                System.out.println("Ayam Goreng");
+                break;
+            case 2:
+                System.out.println("Bubur Ayam");
+                break;
+            case 3:
+                System.out.println("Bakso");
+                break;
+            case 4:
+                System.out.println("Ikan Bakar");
+        }
+    }
+}
