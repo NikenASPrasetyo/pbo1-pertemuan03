e835e16fa06b91a614eb574aad2078e31de12d60
NikenASPrasetyo <nikenasprasetyo@gmail.com>
2020-04-13 15:07:08 +0800

    If Else Statement 1 true

---
+++ src/percabangan/PercabanganIFELSE.java
@@ -0,0 +1,14 @@
+package percabangan;
+
+public class PercabanganIFELSE {
+    public static void main(String[] args) {
+        double belanja = 75000;
+        double dompet = 73000;
+
+        if(dompet<belanja) {
+            System.out.println("Biaya masih kurang, lain kali ya!");
+        }else{
+            System.out.println("Yeay! Uang cukup! Belanja secukupnya saja yaa~ jangan lupa menabung lagi!");
+        }
+    }
+}
